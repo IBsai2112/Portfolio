3cf8d09571fd51680f9ed7abe0415a697f962ab9
Sainjal Kalnekar <sainjalkalnekar21@gmail.com>
2026-02-16 20:47:01 +0530

    Loader addition

---
+++ components/Loader.jsx
@@ -0,0 +1,114 @@
+"use client";
+import { motion } from 'framer-motion';
+import React from 'react';
+import styled from 'styled-components';
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+
+const Container = styled(motion.div)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+touch-action: none;
+overflow: hidden;
+  width: 100vw;
+  height: 100vh;
+
+  z-index: 6;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  background-color: #242424;
+
+  width: 100%;
+
+  @media (max-width: 48em) {
+    svg{
+      width: 20vw;
+    }
+  }
+
+  svg {
+    width: 10vw;
+
+    height: auto;
+    overflow: visible;
+    stroke-linejoin: round;
+    stroke-linecap: round;
+    g {
+      path {
+        stroke: #ffffff;
+      }
+    }
+  }
+`;
+
+const pathVariants = {
+  hidden: {
+    opacity: 0,
+    pathLength: 0,
+  },
+  visible: {
+    opacity: 1,
+    pathLength: 1,
+
+    transition: {
+      duration: 2,
+      // yoyo: Infinity,
+      ease: 'easeInOut',
+    },
+  },
+};
+const textVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+
+    transition: {
+      duration: 1,
+      yoyo: Infinity,
+
+      ease: 'easeInOut',
+    },
+  },
+};
+
+const Text = styled(motion.span)`
+  font-size: ${(props) => props.theme.fontxl};
+  color: ${(props) => props.theme.text};
+  padding-top: 0.5rem;
+
+  @media (max-width: 48em) {
+    font-size: ${(props) => props.theme.fontlg};
+
+  }
+`;
+
+const Loader = () => {
+  return (
+    <Container
+      initial={{ y: 0, opacity: 1 }}
+      exit={{ y: '100%', opacity: 0 }}
+      transition={{ duration: 2 }}
+    >
+      {/* <img src={star} alt="Wibe Fashion" /> */}
+      <DotLottieReact
+  src="https://lottie.host/575ed603-9bcc-49e4-b4c9-1bf425f09940/MwIj8U3OXA.lottie"
+  loop
+  autoplay
+  style={{ width: "120px", height: "120px" }}
+/>
+      <Text variants={textVariants} initial="hidden" animate="visible">
+        Sai Studio
+      </Text>
+    </Container>
+  );
+};
+
+export default Loader;
